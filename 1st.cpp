7763450c30ce57a82fb43e75447c68f73fdac250
Mahan MI <majlesi.mahan@gmail.com>
2023-10-20 12:37:08 +0330

    inital commit

---
+++ 1st.cpp
@@ -0,0 +1,11 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! \n Mobiiiiin Konie";
+ 
+    return 0;
+}
---
+++ 2nd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  int a = 5;
+  int b = 6;
+  int tmp = a;
+  a = b;
+  b = tmp;
+  cout << "a = " << a << endl;
+  return 0;
+}
