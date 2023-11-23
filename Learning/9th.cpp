95f96eb73de61df8ae9b3cca9b4456b24097a606
Mahan MI <majlesi.mahan@gmail.com>
2023-11-23 18:48:47 +0330

    added 9 Learning

--- Learning/9th.cpp
+++ Learning/9th.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
 int main(int argc, char const *argv[])
 {
-  std::cout << "test" << std::endl;
+  int n;
+  cout << "Enter the size of the array: ";
+  cin >> n;
+
+  int array[n];
+  cout << "Enter " << n << " elements: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> array[i];
+  }
+
+  int array0[] = array[];
+
+  cout << "Array elements: ";
+  for (int i = 0; i < n; i++)
+  {
+    cout << array0[i] << " ";
+  }
+
   return 0;
 }
