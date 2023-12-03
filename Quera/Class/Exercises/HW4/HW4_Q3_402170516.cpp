30a85c986d7c2a412f3ae1f6bc1cbdc1f1d8c1af
Mahan MI <majlesi.mahan@gmail.com>
2023-12-03 23:10:32 +0330

    HW4 Q3 Solved

--- Quera/Class/Exercises/HW4/HW4_Q3_402170516.cpp
+++ Quera/Class/Exercises/HW4/HW4_Q3_402170516.cpp
@@ -1,66 +1,30 @@
 #include <iostream>
-#define IoFast                  \
-  ios_base::sync_with_stdio(0); \
-  cin.tie(0);
-#include </Users/mahan/Documents/VS Code/C++/Library/myheader.h>
 using namespace std;
-/* void sortArray(int a[], int from, int to)
-{
-  bool changed = true;
-  for (int i = to - 1; (i > 0) && (changed); i--)
-  {
-    changed = false;
-    for (int j = (from - 1); j < i; j++)
-      if (a[j] > a[j + 1])
-      {
-        swap(a[j], a[j + 1]);
-        changed = true;
-      }
-  }
-} */
 int main()
 {
-  IoFast;
-  int quantity, questions, iAns = 0;
-  cin >> quantity >> questions;
-  int *productID = new int[quantity];
-  int *productID_sorted = new int[quantity];
-  int *answers = new int[questions];
-  //// int productID[quantity], productID_sorted[quantity], answers[questions];
-  for (int i = 0; i < quantity; i++)
+  int products, questions, answer = 0;
+  cin >> products >> questions;
+  int answers[100000] = {0};
+  bool isDifferent[100000];
+  int productID[products];
+  for (int i = 0; i < products; i++)
   {
-    int input;
-    cin >> input;
-    productID[i] = input;
-    productID_sorted[i] = input;
+    cin >> productID[i];
   }
-  timer(start);
-  for (int j = 0; j < questions; j++)
+  for (int i = products - 1; i >= 0; i--)
   {
-    int i, ans = 1;
-    cin >> i;
-    sortArray(productID_sorted, i, quantity);
-    for (int j = (i - 1); j < quantity; j++)
+    if (!(isDifferent[productID[i] - 1]))
     {
-      if (productID_sorted[j] < productID_sorted[j + 1])
-      {
-        ans++;
-      }
+      isDifferent[productID[i] - 1] = true;
+      answer++;
     }
-    answers[iAns] = ans;
-    iAns++;
-    //// memcpy(productID_sorted, productID, quantity * sizeof(int));
-    for (int j = (i - 1); j < quantity; j++)
-    {
-      productID_sorted[j] = productID[j];
-    }
+    answers[i] = answer;
   }
   for (int i = 0; i < questions; i++)
   {
-    cout << answers[i] << endl;
+    int input;
+    cin >> input;
+    cout << answers[input - 1] << endl;
   }
-
-  timer(stop);
-  cout << "Time taken by function: " << Duration(start, stop) << " milliseconds" << endl;
   return 0;
 }
