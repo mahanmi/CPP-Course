56ff6c6c93185334e21196ad98b5085cf7147521
Mahan MI <majlesi.mahan@gmail.com>
2023-12-17 13:32:11 +0330

    updated Library

--- Library/myheader.h
+++ Library/myheader.h
@@ -43,17 +43,17 @@ void printArray(int array[], int n)
   cout << "\b\b}" << endl;
 }
 // ###################################################################
-//
-void swap(long long int &a, long long int &b)
+//                             Swap
+void swap(int &a, int &b)
 {
-  long long int temp;
+  int temp;
   temp = a;
   a = b;
   b = temp;
 }
 // ###################################################################
-//
-void sort(long long int &a, long long int &b, long long int &c)
+//                            sort 3 items
+void sort(int &a, int &b, int &c)
 {
   for (int i = 0; i < 2; i++)
   {
@@ -68,8 +68,8 @@ void sort(long long int &a, long long int &b, long long int &c)
   }
 }
 // ###################################################################
-//
-void sortInverse(long long int &a, long long int &b, long long int &c)
+//                         sort 3 items inverse
+void sortInverse(int &a, int &b, int &c)
 {
   for (int i = 0; i < 2; i++)
   {
@@ -80,6 +80,30 @@ void sortInverse(long long int &a, long long int &b, long long int &c)
     if (c > b)
     {
       swap(b, c);
+    }
+  }
+}
+// ###################################################################
+//                       Matrix Multiply
+void matrixMultiply(long long int matrix1[][50], long long int matrix2[][50], int n)
+{
+  long long int result[50][50];
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i][j] = 0;
+      for (int k = 0; k < n; k++)
+      {
+        result[i][j] += ((matrix1[i][k]) * (matrix2[k][j]));
+      }
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      matrix1[i][j] = result[i][j];
     }
   }
 }
