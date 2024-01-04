0628de5b3cd2da70758a40989a152c537cde8ae6
Mahan MI <majlesi.mahan@gmail.com>
2024-01-05 02:33:22 +0330

    HW6 Q3 Added

---
+++ SDL/src/Quera/HW6/HW6_Q3_402170516.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include "SDL_Headers.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int WIDTH = 1200, HEIGHT = 960;
+  // Initialize SDL
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); // Initialize SDL
+  Mix_Init(MIX_INIT_MP3);                    // Initialize SDL_mixer
+  IMG_Init(IMG_INIT_PNG);                    // Initialize SDL_image
+
+  SDL_Window *window = SDL_CreateWindow(
+      "Q3",                    // Window title
+      SDL_WINDOWPOS_UNDEFINED, // Initial x position
+      SDL_WINDOWPOS_UNDEFINED, // Initial y position
+      WIDTH,                   // Width, in pixels
+      HEIGHT,                  // Height, in pixels
+      SDL_WINDOW_SHOWN         // Flags
+  );
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_Event event;
+  // end of initialization
+  srand(time(0));
+  int R1 = 50, R2 = 75, mod1_x = WIDTH - 2 * R1, mod1_y = HEIGHT - 2 * R1, mod2_x = WIDTH - 2 * R2, mod2_y = HEIGHT - 2 * R2;
+  double Vx_R1 = rand() % 8 + 1, Vy_R1 = rand() % 8 + 1, X0_R1 = (rand() % mod1_x) + R1, Y0_R1 = (rand() % mod1_y) + R1, R_R1 = 0, G_R1 = 255, B_R1 = 255;
+  double Vx_R2 = rand() % 8 + 1, Vy_R2 = rand() % 8 + 1, X0_R2 = (rand() % mod2_x) + R2, Y0_R2 = (rand() % mod2_y) + R2, R_R2 = 255, G_R2 = 0, B_R2 = 255;
+  while (sqrt(pow(X0_R1 - X0_R2, 2) + pow(Y0_R1 - Y0_R2, 2)) <= R1 + R2)
+  {
+    X0_R2 = (rand() % mod2_x) + R2, Y0_R2 = (rand() % mod2_y) + R2;
+  }
+  bool running = true, paused = false;
+  // Initialize SDL_mixer
+  Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+  // Load sound file
+  Mix_Chunk *sound = Mix_LoadWAV("/Users/mahan/Documents/VS Code/C++/SDL/src/Quera/HW6/ZN.mp3");
+  while (running)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_KEYDOWN)
+      {
+        int key = event.key.keysym.sym;
+        if (key == SDLK_ESCAPE)
+          running = false;
+        if (key == SDLK_p)
+          paused = !paused;
+        if (key == SDLK_UP || key == SDLK_SPACE)
+          Vx_R1 *= 1.5, Vy_R1 *= 1.5, Vx_R2 *= 1.5, Vy_R2 *= 1.5;
+        if (key == SDLK_DOWN)
+          Vx_R1 *= 0.5, Vy_R1 *= 0.5, Vx_R2 *= 0.5, Vy_R2 *= 0.5;
+        if (key == SDLK_i)
+          Mix_PlayChannel(-1, sound, 0);
+      }
+    }
+    if (!paused)
+    {
+      if (sqrt(pow(X0_R1 - X0_R2, 2) + pow(Y0_R1 - Y0_R2, 2)) <= R1 + R2)
+      {
+
+        // Mix_HaltChannel(-1);
+        Mix_PlayChannel(-1, sound, 0);
+        X0_R1 -= Vx_R1, Y0_R1 -= Vy_R1, X0_R2 -= Vx_R2, Y0_R2 -= Vy_R2;
+        double Vx_R1_i = Vx_R1, Vy_R1_i = Vy_R1, Vx_R2_i = Vx_R2, Vy_R2_i = Vy_R2;
+        Vx_R1 = 0.2 * Vx_R1_i + 0.8 * Vx_R2_i, Vy_R1 = 0.8 * Vy_R2_i + 0.2 * Vy_R1_i, Vx_R2 = -0.2 * Vx_R2_i + 1.2 * Vx_R1_i, Vy_R2 = 1.2 * Vy_R1_i - 0.2 * Vy_R2_i;
+      }
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+      SDL_RenderClear(renderer);
+      filledEllipseRGBA(renderer, X0_R1, Y0_R1, R1, R1, R_R1, G_R1, B_R1, 255);
+      filledEllipseRGBA(renderer, X0_R2, Y0_R2, R2, R2, R_R2, G_R2, B_R2, 255);
+      SDL_RenderPresent(renderer);
+      Uint32 FPS60 = 1000 / 60;
+      SDL_Delay(FPS60);
+      if (X0_R1 >= WIDTH - R1 || X0_R1 <= R1)
+      {
+        X0_R1 -= Vx_R1;
+        Vx_R1 *= -1;
+        std::cout << "Vx=" << Vx_R1 << " Vy=" << Vy_R1 << " X=" << X0_R1 << " Y=" << Y0_R1 << std::endl;
+      }
+      if (Y0_R1 >= HEIGHT - R1 || Y0_R1 <= R1)
+      {
+        Y0_R1 -= Vy_R1;
+        Vy_R1 *= -1;
+        std::cout << "Vx=" << Vx_R1 << " Vy=" << Vy_R1 << " X=" << X0_R1 << " Y=" << Y0_R1 << std::endl;
+      }
+      if (X0_R2 >= WIDTH - R2 || X0_R2 <= R2)
+      {
+        X0_R2 -= Vx_R2;
+        Vx_R2 *= -1;
+        std::cout << "Vx=" << Vx_R2 << " Vy=" << Vy_R2 << " X=" << X0_R2 << " Y=" << Y0_R2 << std::endl;
+      }
+      if (Y0_R2 >= HEIGHT - R2 || Y0_R2 <= R2)
+      {
+        Y0_R2 -= Vy_R2;
+        Vy_R2 *= -1;
+        std::cout << "Vx=" << Vx_R2 << " Vy=" << Vy_R2 << " X=" << X0_R2 << " Y=" << Y0_R2 << std::endl;
+      }
+      X0_R1 += Vx_R1, Y0_R1 += Vy_R1;
+      X0_R2 += Vx_R2, Y0_R2 += Vy_R2;
+    }
+  }
+  Mix_FreeChunk(sound);
+  Mix_CloseAudio();
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  IMG_Quit();
+  SDL_Quit();
+  return 0;
+}
